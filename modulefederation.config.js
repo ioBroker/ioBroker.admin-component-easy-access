392f1725a408ee4b2829fadce93744d42b50e4c4
GermanBluefox <dogafox@gmail.com>
2022-06-17 09:46:45 +0200

    * (bluefox) Corrected issue with version of common packages

--- modulefederation.config.js
+++ modulefederation.config.js
@@ -13,27 +13,9 @@ const makeShared = pkgs => {
 
 module.exports = {
     name: 'ConfigCustomEasyAccessSet',
-    // library: { type: 'module' },
-    // library: { type: 'var', name: 'MaterialDemo' },
     filename: 'customComponents.js',
     exposes: {
         './Components': './src/Components.jsx',
     },
-    shared: makeShared(['@mui/material', '@mui/styles', '@iobroker/adapter-react-v5', 'react', 'react-dom', 'prop-types'])
-    // shared: {
-    // react: {singleton: true,
-    //     eager: true,
-    //     requiredVersion: deps.react},
-    // 'react-dom': {singleton: true,
-    //     requiredVersion: deps.react['react-dom']},
-    // '@mui/material': {singleton: true},
-    // '@mui/icons-material': {singleton: true},
-    // 'prop-types': {singleton: true},
-    // '@iobroker/adapter-react-v5': {singleton: true},
-    // '@mui/styles': {singleton: true},
-    // 'react-ace': {singleton: true},
-    // }
-    // './src/visRxWidget.jsx': {
-    //     packageName: 'visRxWidget',
-    // },
+    shared: makeShared(['@mui/material', '@mui/styles', '@iobroker/adapter-react-v5', 'react', 'react-dom', 'prop-types']),
 };
